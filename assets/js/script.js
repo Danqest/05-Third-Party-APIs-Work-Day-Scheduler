fb0dac7a1392d2afac1b1e85ce259f8ad4f2719a
Colin Brooks <mcbrooks@pm.me>
2022-04-04 23:25:01 -0400

    added logic for color coding & time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,30 @@
 var today = moment()
+var times = [9,10,11,12,13,14,15,16,17]
 
 $('#currentDay').text(today.format("dddd, MMMM Do, YYYY"))
+
+
+
+function init() {
+    updateFormColors()
+}
+
+function updateFormColors() {
+    for (let i = 0; i < $('.form-control').length; i++) {
+        if (times[i] > moment().hours()) {
+            $('.form-control').eq(i).addClass('future')
+        }
+        else if (times[i] == moment().hours()) {
+            $('.form-control').eq(i).addClass('present')
+        }
+        else if (times[i] < moment().hours()) {
+            $('.form-control').eq(i).addClass('past')
+        }
+    }
+}
+
+
+
+
+
+init()
